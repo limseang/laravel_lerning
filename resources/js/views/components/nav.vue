9a512a22be628ce943131ae45416a974c2015020
limseang <povlimseang.com@gmail.com>
2022-08-09 10:49:50 +0700

    Update nav.vue

--- resources/js/views/components/nav.vue
+++ resources/js/views/components/nav.vue
@@ -8,53 +8,93 @@
           </div>
           <div class="mt-8">
             <!-- Secondary navigation -->
-           
+              <a v-for="item in Navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:text-gray-900 hover:bg-gray-50', 'group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md']" :aria-current="item.current ? 'page' : undefined">
+                      <component :is="item.icon" :class="[item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500', 'mr-3 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
+                      {{ item.name }}
+                    </a>
+                
             <br>
+             <h3 class="px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider" id="desktop-teams-headline">Teams</h3>
             <div class="mt-1 space-y-1" role="group" aria-labelledby="desktop-teams-headline">
-              <a v-for="team in teams"  @click="teams" class="group flex items-center px-3 py-2 text-sm font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50">
+              <a v-for="team in teams" :key="team.name" :href="team.href" class="group flex items-center px-3 py-2 text-sm font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50">
                 <span :class="[team.bgColorClass, 'w-2.5 h-2.5 mr-4 rounded-full']" aria-hidden="true" />
                 <span class="truncate">
                   {{ team.name }}
                 </span>
               </a>
             </div>
-          </div>
+              </div>
+            
         </nav>
         
      
 </template>
 
-<script>
-import user from '../User.vue'
-import { useRouter } from 'vue-router'
-import { AdjustmentsIcon, ClockIcon, HomeIcon, MenuAlt1Icon, ViewListIcon, XIcon } from '@heroicons/vue/outline'
-export default {
-  name: 'Nav',
+<script setup>
+import { ref } from 'vue'
+import {
+  Dialog,
+  DialogPanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  TransitionChild,
+  TransitionRoot,
+} from '@headlessui/vue'
+import { ClockIcon, HomeIcon, MenuAlt1Icon, UserIcon, ViewListIcon, XIcon } from '@heroicons/vue/outline'
+import { ChevronRightIcon, DotsVerticalIcon, SearchIcon, SelectorIcon } from '@heroicons/vue/solid'
 
- }
-
-
-    const teams = [
+const navigation = [
+  { name: 'Home', href: '/', icon: HomeIcon, current: true },
+  { name: 'user', href: '/admin/user', icon: UserIcon, current: false },
+  { name: 'Recent', href: '#', icon: ClockIcon, current: false },
+]
+const teams = [
   { name: 'Engineering', href: '#', bgColorClass: 'bg-indigo-500' },
   { name: 'Human Resources', href: '#', bgColorClass: 'bg-green-500' },
   { name: 'Customer Success', href: '#', bgColorClass: 'bg-yellow-500' },
 ]
-const router = useRouter()
-
-function team() {
-
-      router.push({
-        name: 'team',
-        path:'/admin/team/',
-         
-       
-      })
-    }
-
-
-const navigation = [
-  { name: 'Home', href: '#', icon: HomeIcon, current: true },
-  { name: 'My tasks', href: '#', icon: ViewListIcon, current: false },
-  { name: 'Recent', href: '#', icon: ClockIcon, current: false },
+const projects = [
+  {
+    id: 1,
+    title: 'GraphQL API',
+    initials: 'GA',
+    team: 'Engineering',
+    members: [
+      {
+        name: 'Dries Vincent',
+        handle: 'driesvincent',
+        imageUrl:
+          'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        name: 'Lindsay Walton',
+        handle: 'lindsaywalton',
+        imageUrl:
+          'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        name: 'Courtney Henry',
+        handle: 'courtneyhenry',
+        imageUrl:
+          'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        name: 'Tom Cook',
+        handle: 'tomcook',
+        imageUrl:
+          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    ],
+    totalMembers: 12,
+    lastUpdated: 'March 17, 2020',
+    pinned: true,
+    bgColorClass: 'bg-pink-600',
+  },
+  // More projects...
 ]
+const pinnedProjects = projects.filter((project) => project.pinned)
+
+const sidebarOpen = ref(false)
 </script>
